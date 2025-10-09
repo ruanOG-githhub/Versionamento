8b5b246a26e6009db7ae576b2a5a4ff1757bd537
LarissaBNogueira <larissaboninogueiraa@gmail.com>
2025-10-09 09:17:18 -0300

    aula

---
+++ Larissa/006.py
@@ -0,0 +1,13 @@
+mensagem = input("Digite uma mensagem: ")
+
+mensagem_maiuscula = mensagem.lower()
+
+print("Mensagem em maiúsculas:", mensagem_maiuscula)
+
+
+
+mensagem = input("Digite uma mensagem: ")
+
+mensagem_minusculas = mensagem.lower()
+
+print("Mensagem em minusculas:", mensagem_minusculas)
