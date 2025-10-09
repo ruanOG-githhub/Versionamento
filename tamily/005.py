505064a5003c68dcee01d2aeae1381841b9719d5
tamilyvt <tamily702@gmail.com>
2025-10-09 09:26:31 -0300

    h

---
+++ tamily/005.py
@@ -0,0 +1,7 @@
+#Lista com os nomes das pessoas da familia 
+familia = ["Carmosa", "Ernande", "Fernanda", "Tãmily"]
+# Coloca os nomes em ordem alfabetica 
+familia.sort()
+print("Nome da familia em ordem alfabetica:")
+for nome in familia:
+   print (nome) 
