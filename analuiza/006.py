65f861d72304174fb39abcf2eb2f35d6af83d209
renatinx <ana.7421848@aluno.mg.gov.br>
2025-10-09 09:05:53 -0300

    Update 006.py

--- analuiza/006.py
+++ analuiza/006.py
@@ -1,5 +1,3 @@
 mensagem = input("Digite uma mensagem: ")
 
-mensagem_maiuscula = mensagem.upper()
-
-print("Mensagem em maiúsculas:", mensagem_maiuscula)
+print("Mensagem em maiúsculas:", mensagem.upper())
