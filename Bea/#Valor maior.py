a3a30abad1fe90f89ba91a9f959a482b44ea758e
joaopedro10modesto-collab <beatrizesclagh@gmail.com>
2025-10-21 08:25:44 -0300

    jg

---
+++ Bea/#Valor maior.py
@@ -0,0 +1,8 @@
+#Valor maior 
+
+valor1 = int(input("Digite o primeiro valor:"))
+valor2 = int(input("Digite o segundo valor:"))
+
+resultado = f"O maior valor é o primeiro!" if valor1 > valor2 else (if"o maior valor é o segundo!"
+
+print(resultado)
