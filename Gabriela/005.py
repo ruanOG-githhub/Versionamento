23a16c2d792ff5965367963a73b5713b4bdceb9e
renatinx <renatincruz122@gmail.com>
2025-10-22 08:09:05 -0300

    Revert "Merge branch 'main' of https://github.com/ruanOG-githhub/Versionamento"
    This reverts commit 2c6663b51d4ef6aa6c07f934d9d4e0990b54a22a, reversing
    changes made to 96c1116662bf0305ab057298522844a6a3d57bf4.

--- Gabriela/005.py
+++
@@ -1,7 +0,0 @@
-familia = ["João", "Gislaine", "Hugo"]
-
-familia.sort()
-
-print("Nomes da família em ordem alfabética:")
-for nome in familia:
-    print(nome)
--- Gabriela/006.py
+++
@@ -1,5 +0,0 @@
-mensagem = input("Digite uma mensagem qualquer: ")
-
-mensagem_maiuscula = mensagem.lower()
-
-print("Mensagem em maiúsculas:", mensagem_maiuscula)
