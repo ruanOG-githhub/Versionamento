68d1f73274a1c7d6c696094941ebbae38cc70700
beatrizhubgit <beatrizesclagh@gmail.com>
2025-10-29 11:08:43 -0300

    alguma coisa

---
+++ Bea/008.py
@@ -0,0 +1,15 @@
+frutas = []
+print("Digite o nome das frutas solicitadas:")
+laranja = input("Digite 'laranja': ")
+limao = input("Digite 'limão': ")
+banana = input("Digite 'banana': ")
+pera = input("Digite 'pera': ")
+uva = input("Digite 'uva': ")
+frutas.append(laranja)   
+frutas.append(limao)     
+frutas.append(banana)    
+frutas.append(pera)      
+frutas.append(uva)       
+print("\nLista de frutas na ordem definida:")
+for i, fruta in enumerate(frutas, start=1):
+    print(f"{i}º {fruta}")
