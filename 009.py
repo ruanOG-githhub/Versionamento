7d56f8d4c8601efbe917f75efa40df47cbb1986f
isabellybatista445566-beep <isabellybatista445566@gmail.com>
2025-10-29 11:03:14 -0300

    Revert "Update 009.py"
    This reverts commit 269195d61cafd94ce3f5a0d02cfe304dea535900.

--- 009.py
+++ 009.py
@@ -1,6 +1,6 @@
 nome_completo = input("Digite seu nome completo: ").strip()
 partes_nome = nome_completo.split()
-if  print(partes_nome) > 1:
+if len(partes_nome) > 1:
     print("Seu sobrenome é:", partes_nome[-1])
 else:
     print("Você digitou apenas um nome, não há sobrenome.")
